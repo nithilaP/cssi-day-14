83702ec4cb454c6b10728bbd1ab9cc24e9dd2757
Nithila_Poongo <nithila@poongo.net>
2021-07-29 10:21:14 -0700

    Add Peter.

--- testing_is_necessary.js
+++ testing_is_necessary.js
@@ -12,6 +12,12 @@ funAnimals.push({
     reason: "four legs worth of energy at the cost of three"
 })
 
+funAnimals.push({
+    name: "Peter",
+    type: "Tortoies",
+    reason: "Backyard Grass Eater"
+})
+
 for (let animal of funAnimals) {
     console.log(`${animal.name} the ${animal.type} is fun.  Reason: ${animal.reason}`)
 }
